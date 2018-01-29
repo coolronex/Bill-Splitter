db6a64e705c6d0de454f812c1188684f4cc6aaec
Aaron <heyitsronex@gmail.com>
2018-01-28 22:31:12 -0500

    added tip percentage & changed design

--- Bill Splitter/ViewController.m
+++ Bill Splitter/ViewController.m
@@ -10,13 +10,19 @@
 
 @interface ViewController () <UITextFieldDelegate>
 
+
 @property (weak, nonatomic) IBOutlet UITextField *totalBillTextField;
+@property (weak, nonatomic) IBOutlet UITextField *tipTextField;
 @property (weak, nonatomic) IBOutlet UISlider *numberOfPerson;
 @property (weak, nonatomic) IBOutlet UILabel *amountForEachPersonLabel;
 @property (weak, nonatomic) IBOutlet UILabel *sliderCurrentValueLabel;
 
 @property (nonatomic, strong) NSDecimalNumber *totalBillAmount;
+@property (nonatomic, strong) NSDecimalNumber *totalTipAmount;
+@property (nonatomic, strong) NSDecimalNumber *totalBillWithTip;
+@property (nonatomic, strong) NSDecimalNumber *tipPercentage;
 @property (nonatomic, strong) NSDecimalNumber *sliderValue;
+
 
 @end
 
@@ -24,21 +30,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //sender.value sends current value(float)
+    //then i convert it to NSNumber (idk how to convert directly from float to NSDecimal number)
+    NSNumber *currentSliderValue = [[NSDecimalNumber alloc] initWithInteger: self.numberOfPerson.value];
+    
+    //converting currentSliderValue into NSDecimal to be used for calculations
+    NSDecimalNumber *convertedCurrentSliderValue = [NSDecimalNumber decimalNumberWithDecimal:currentSliderValue.decimalValue];
+    self.sliderValue = convertedCurrentSliderValue;
 
 }
-
 
 - (IBAction)calculateSplitAmount:(UIButton *)sender {
 
     //if user input in textfield has no value then it's auto set to 0
     if ([self.totalBillTextField.text isEqualToString:@""]) {
-        
         self.totalBillTextField.text = [NSString stringWithFormat:@"0"];
     }
-        
+    
     //get string from textField and convert to NSDecimalNumber
     self.totalBillAmount = [[NSDecimalNumber alloc] initWithString:self.totalBillTextField.text];
-    NSLog(@"Total: %@", self.totalBillAmount);
+    NSLog(@"Bill amount: $%@", self.totalBillAmount);
+    self.tipPercentage = [[NSDecimalNumber alloc] initWithString:self.tipTextField.text];
+    NSLog(@"Tip: %@%%", self.tipPercentage);
+    
+    
     
     //handler for NSDecimal dividing method
     NSDecimalNumberHandler *handler = [NSDecimalNumberHandler decimalNumberHandlerWithRoundingMode:NSRoundBankers
@@ -48,28 +64,34 @@
                                                                                   raiseOnUnderflow:NO
                                                                                raiseOnDivideByZero:NO];
     //calculations
-    self.totalBillAmount = [self.totalBillAmount decimalNumberByDividingBy:self.sliderValue withBehavior:handler];
+    NSDecimalNumber *tip = [self.tipPercentage decimalNumberByDividingBy:[NSDecimalNumber decimalNumberWithString:@"100"]];
+    NSDecimalNumber *totalBillWithTip = [self.totalBillAmount decimalNumberByAdding: tip]; //adding bill with tip
+    NSDecimalNumber *eachPersonTotal = [totalBillWithTip decimalNumberByDividingBy:self.sliderValue withBehavior:handler]; //divide equally among everyone
+    NSLog(@"Tip: %@", tip);
+    NSLog(@"Bill + tip: $%@", totalBillWithTip);
+    NSLog(@"Number of person: %@", self.sliderValue);
+    NSLog(@"Total after calculations: $%@", eachPersonTotal);
 
-    NSLog(@"Total after calculations: %@", self.totalBillAmount);
-    NSLog(@"Current value of slider: %@", self.sliderValue);
-    
-    self.amountForEachPersonLabel.text = [NSString stringWithFormat:@"Total for each person is: $%@", self.totalBillAmount];
+    self.amountForEachPersonLabel.text = [NSString stringWithFormat:@"Total for each person is: $%@", eachPersonTotal];
 }
 
 
 - (IBAction)sliderMoved:(UISlider *)sender {
     
-    //sender.value sends current value(float)
-    //then i convert it to NSNumber (idk how to convert directly from float to NSDecimal number)
-    NSNumber *currentSliderValue = [[NSDecimalNumber alloc] initWithInteger:sender.value];
+    //self.numberOfPerson.value has value in float that i want to convert to NSDecimalNumber
+    //convert the current slider value to NSNumber (idk how to convert directly from float to NSDecimal number)
+    NSNumber *currentSliderValue = [[NSDecimalNumber alloc] initWithInteger: self.numberOfPerson.value];
     
     //converting currentSliderValue into NSDecimal to be used for calculations
     NSDecimalNumber *convertedCurrentSliderValue = [NSDecimalNumber decimalNumberWithDecimal:currentSliderValue.decimalValue];
     self.sliderValue = convertedCurrentSliderValue;
     
     //label will self update because live continous events of slider is set ON through Interface Builder
-    self.sliderCurrentValueLabel.text = [NSString stringWithFormat:@"Number of person: %@", currentSliderValue];
+    self.sliderCurrentValueLabel.text = [NSString stringWithFormat:@"Number of person: %@", self.sliderValue];
+    
 }
+
+
 
 
 @end
